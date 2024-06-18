6e529b91e056d1312250c144c5ee91816662d106
marconcha <marconchaga@gmail.com>
2024-06-18 19:26:15 +0200

    Quitamos filtros de la página de datos

--- pages/datos.py
+++ pages/datos.py
@@ -4,33 +4,19 @@ import utils
 #* --------------------Asignamos la configuracion de la página  ------------------#
 st.set_page_config(**utils.confPage)
 
-#------------------Cargar Datos----------------------#
-# Definir una función para cargar los datos
-
-# Definir una función para cargar los datos
-
 
 if 'df' in st.session_state and 'df_agrupado' in st.session_state:
-
     df = st.session_state.df.copy()
-
-
     df_agrupado = st.session_state.df_agrupado.copy()
-
-
     # --------------------SIDEBAR----------------------------#
     utils.menu() #   👈
 
     #--------------------  TITULO PAGINA DE DATOS  ----------------------------#
     st.title("Datos usados en el análisis")
 
-    #--------------------  BARRA LATERAL  ----------------------------#
-    df = utils.filtros([],df)
-    # df_agrupado = utils.filtros(['Año','Día semana','Distrito','Tipo accidente','Tramo horario', 'Estado meteorológico'],df_agrupado)
-
     #--------------------------DFS---------------------------#
     #mostrar df sin agrupar
-    st.subheader("Data frame original")
+    st.subheader("Data frame Implicados")
     st.write(df)
-    st.subheader("Data frame agrupado")
+    st.subheader("Data frame Accidentes")
     st.write(df_agrupado)
